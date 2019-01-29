68e5e6639049a64812e1856570f03d5cd896d993
david lang <david.lang697@gmail.com>
2019-01-29 18:45:58 +0100

    Correction scenario : les deux transitions étaient allaient vers le meme etat

--- extendedGroovy/scripts/StateBasedAlarm.groovy
+++ extendedGroovy/scripts/StateBasedAlarm.groovy
@@ -6,7 +6,7 @@ state "on" means LED1 becomes high
 state "off" means LED1 becomes low
 
 from on to off when button1 becomes high
-from on to off when button1 becomes high
+from off to on when button1 becomes high
 
 initial off
 
