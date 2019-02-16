8c87cd345d023c76902db0ec894dd3c35c2f1a22
Lucas Matteo <lucas.matteo@etu.unice.fr>
2019-02-16 10:41:02 +0100

    updating display

--- gui/GraphComponent.py
+++ gui/GraphComponent.py
@@ -1,16 +1,17 @@
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-xdata = []
-ydata = []
+
 
 class GraphComponent:
     def __init__(self,master):
         fig = Figure()
-        sub = fig.add_subplot(111)
-        sub.set_ylim(-0.1,1.1)
-        sub.set_xlim(0)
-        line, = sub.plot(xdata, ydata,'-r')
+        self.sub = fig.add_subplot(111)
+        self.sub.set_ylim(-0.1,1.1)
+
+        self.xdata = []
+        self.ydata = []
+        line, = self.sub.plot(self.xdata, self.ydata,'-r')
         self.line = line
         self.canvas = FigureCanvasTkAgg(fig, master=master)
         self.canvas.draw()
@@ -19,7 +20,20 @@ class GraphComponent:
             return self.canvas.get_tk_widget()
 
     def update(self,data):
-        self.line.set_ydata(ydata.append(1))
+        self.ydata.extend(data[1])
+        self.xdata.extend(data[0])
+
+        if len(self.xdata) > len(self.ydata):
+            self.xdata = self.xdata[0:len(self.ydata)]
+        else :
+            self.ydata = self.ydata[0:len(self.xdata)]
+
+        self.line.set_data(self.xdata, self.ydata)
+        #self.line.set_ydata(ydata.append(1))
+        maxlim = self.xdata[len(self.xdata)-1]
+        lowerlim = maxlim-10000 if maxlim > 10000 else 0
+
+        self.sub.set_xlim(lowerlim, maxlim)
         self.canvas.draw()
         self.canvas.flush_events()
 
