1e94c4b6ecec764fed316496ae3df3ad08a9dbcd
David LANG <david.lang697@gmail.com>
2019-02-17 17:41:30 +0100

    Fix "brickId" and "BrickId" different

--- kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/generator/config/TextConfig.java
+++ kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/generator/config/TextConfig.java
@@ -2,17 +2,8 @@ package io.github.mosser.arduinoml.kernel.generator.config;
 
 public class TextConfig extends BrickConfig {
     public final String type = "Text";
-    public String BrickId;
 
     public String getType() {
         return type;
-    }
-
-    public String getBrickId() {
-        return BrickId;
-    }
-
-    public void setBrickId(String brickId) {
-        BrickId = brickId;
     }
 }
